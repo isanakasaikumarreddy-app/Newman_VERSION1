1881c3ae266d0b6359cbad54f1503ec5ecf46c48
Isanaka Sai Kumar Reddy <isanaka.saikumar@gmail.com>
2026-04-28 01:19:12 +0530

    Fix API proxy redirect

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/",
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:5173/",
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ""),
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
